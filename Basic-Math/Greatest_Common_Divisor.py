0361e95caac3feb2f7af3f46cbe5edec4c5fbb9c
Vardhan <ksaivardhan9@gmail.com>
2026-05-23 21:45:05 +0530

    Completed Greatest Commom Divisor

---
+++ Basic-Math/Greatest_Common_Divisor.py
@@ -0,0 +1,22 @@
+# Euclidean ALgorithm for optimized version
+
+
+# according to Euclidean Algo gcd(n1,n2) = gcd(n1-n2, n2)
+
+
+class Solution:
+    def gcd(self, a, b):
+        # code here
+        
+        while a > 0 and b > 0:
+            
+            if a > b:
+                a = a % b
+            else:
+                b = b % a
+                
+        if a == 0:
+            return b
+        else:
+            return a
+                
