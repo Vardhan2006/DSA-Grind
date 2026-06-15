f8c0ad71148c8a78dca2eb03317dc314f8e3fbf9
Vardhan <ksaivardhan9@gmail.com>
2026-06-15 16:05:53 +0530

    Solved 2 recursion problems

---
+++ DSA/Recursion/palindrome.py
@@ -0,0 +1,68 @@
+class Solution:
+
+    def helper(self, n, rev, org):
+
+        if n == 0:
+            return org == rev
+
+        digit = n % 10
+        rev = rev * 10 + digit
+        n = n // 10
+
+        return self.helper(n, rev, org)
+
+    def isPalindrome(self, n):
+
+        n = abs(n)
+
+        return self.helper(n, 0, n)
+
+
+'''
+
+Dry Run
+
+Input: n = 121
+
+Call:
+
+isPalindrome(121)
+
+org = 121
+rev = 0
+
+helper(121, 0, 121)
+
+↓
+
+digit = 1
+rev = 1
+n = 12
+
+helper(12, 1, 121)
+
+↓
+
+digit = 2
+rev = 12
+n = 1
+
+helper(1, 12, 121)
+
+↓
+
+digit = 1
+rev = 121
+n = 0
+
+helper(0, 121, 121)
+
+↓
+
+n == 0
+
+return 121 == 121
+
+True
+
+'''
--- DSA/Recursion/reverse_arr.py
+++ DSA/Recursion/reverse_arr.py
@@ -1,3 +1,5 @@
+#Two Pointer
+
 class Solution:
 
     def helper(self, arr, left, right):
@@ -27,5 +29,57 @@ right - 1
 
 Base Condition:
 left >= right
+
+'''
+
+
+# single pointer 
+
+class Solution:
+
+    def helper(self, arr, i):
+
+        n = len(arr)
+
+        if i >= n // 2:
+            return
+
+        arr[i], arr[n - i - 1] = arr[n - i - 1], arr[i]
+
+        self.helper(arr, i + 1)
+
+    def reverseArray(self, arr):
+
+        self.helper(arr, 0)
+
+        return arr
+
+
+'''
+
+Why is it called Single Pointer?
+
+Because we only maintain one pointer:
+
+i = 0 → 1 → 2
+
+The other pointer is computed automatically:
+
+right = n - i - 1
+
+Why is it called Two Pointer?
+
+Because we explicitly maintain:
+
+left = 0 → 1 → 2
+right = 4 → 3 → 2
+
+So:
+
+Two Pointer:
+left, right
+
+Single Pointer:
+i
 
 '''
